eafe9149f75f9c200bc968ae9b5177172278d654
jan_burak <jan@kodeworks.no>
2021-11-30 14:42:20 +0100

    Create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Brand new Sunlit Sea webpage here soon</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
