b765b73d8953584c7f6612d58046db98467c4930
jan_burak <jan@kodeworks.no>
2022-01-26 10:39:07 +0100

    Fix top page poster for ultra wide

--- src/components/molecules/TopPagePoster.tsx
+++ src/components/molecules/TopPagePoster.tsx
@@ -39,22 +39,24 @@ export const TopPagePoster: React.FC<TopPagePosterProps> = (props) => {
   } else {
     return (
       <>
-        <div className="relative min-h-[400px] md:min-h-[600px] lg:min-h-[800px] mt-10">
-          <img
-            src={props.image}
-            alt={props.imageAlt}
-            className="absolute top-0 left-0 object-cover w-full h-full"
-          />
-          <div className="w-full sls-top-page-poster__gradient min-h-[400px] md:min-h-[600px] lg:min-h-[800px]" />
-          <div className="absolute bottom-0 w-full xl:bottom-24 lg:bottom-16 md:bottom-12 sm:bottom-4">
-            <ContentContainer>
-              <h1 className="mt-8 text-6xl text-white font-headline">{props.headingText}</h1>
-              {props.subHeadingText.map((t, i) => (
-                <p className="text-2xl text-white font-headline" key={i}>
-                  {t}
-                </p>
-              ))}
-            </ContentContainer>
+        <div className="bg-black">
+          <div className="relative min-h-[400px] md:min-h-[600px] lg:min-h-[800px] mt-10 max-w-[1900px] mx-auto">
+            <img
+              src={props.image}
+              alt={props.imageAlt}
+              className="absolute top-0 left-0 object-cover w-full h-full"
+            />
+            <div className="w-full sls-top-page-poster__gradient 2xl: min-h-[400px] md:min-h-[600px] lg:min-h-[800px]" />
+            <div className="absolute bottom-0 w-full xl:bottom-24 lg:bottom-16 md:bottom-12 sm:bottom-4">
+              <ContentContainer>
+                <h1 className="mt-8 text-6xl text-white font-headline">{props.headingText}</h1>
+                {props.subHeadingText.map((t, i) => (
+                  <p className="text-2xl text-white font-headline" key={i}>
+                    {t}
+                  </p>
+                ))}
+              </ContentContainer>
+            </div>
           </div>
         </div>
       </>
