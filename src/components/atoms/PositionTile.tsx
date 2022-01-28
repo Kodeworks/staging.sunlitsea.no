da64d2076cf663dc42f988b25e0c495ffbfad887
jan_burak <jan@kodeworks.no>
2022-01-28 15:52:14 +0100

    Fix position tile on mobile

--- src/components/atoms/PositionTile.tsx
+++ src/components/atoms/PositionTile.tsx
@@ -54,16 +54,16 @@ const PositionTile: React.FC<Props> = ({
       nested
     >
       {(close) => (
-        <div className="flex flex-col max-w-screen-xl px-2 mx-2 sm:mx-8 md:px-4 bg-primary-200">
+        <div className="flex flex-col max-w-screen-xl px-1 sm:px-2 sm:mx-8 md:px-4 bg-primary-200">
           <button
             className="self-end text-4xl focus:outline-none active:border-transparent"
             onClick={close}
           >
             &times;
           </button>
-          <div className="flex flex-col px-4 pb-8 text-lg md:px-8 md:flex-row sm:text-xl lg:text-2xl gap-y-2 sm:gap-y-4">
+          <div className="flex flex-col px-1 pb-8 overflow-scroll text-sm text-lg leading-tight sm:px-4 md:px-8 md:flex-row sm:text-xl lg:text-2xl gap-y-2 sm:gap-y-4">
             <div className={'flex flex-col ' + ' ' + (image ? 'md:w-7/12' : '')}>
-              <div className="text-2xl sm:text-3xl lg:text-4xl font-headline">
+              <div className="text-lg leading-none sm:text-3xl lg:text-4xl font-headline">
                 {position.toUpperCase()}
               </div>
               <div>{description}</div>
